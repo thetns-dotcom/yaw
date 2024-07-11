c75ee5c5a610d697e28bcb0872e0402da2b4cab3
TNS - Make your world <jameonwy@gmail.com>
2024-07-11 05:14:39 +0000

    Added gitBase

--- index.js
+++ index.js
@@ -1,41 +1,25 @@
 /**
  * yaw - Self-hosted Git server
+ *
+ * This is the main file of the package
  */
-class Git {
-  constructor(options){
-    this.options = options;
-    this.git = require('simple-git')(this.options.path); // usage: git.add('file.txt');
-    this.def_options = {
-      path: process.cwd(),
-      port: 8080,
-      data: {
-        users: {
-          admin: {
-            password: 'admin',
-            name: 'Admin',
-            email: 'plsgq@example.com'
-          }
-        }
-      }
-    }
-    for(let key in this.def_options){
-      if(this.options[key] === undefined){
-        this.options[key] = this.def_options[key];
-      }
-    }
-    this.users = this.options.data.users;
-    
-  }
-  web(){
-    const express = require('express');
-    const app = express();
-    const bodyParser = require('body-parser');
-    const fs = require('fs');
-    const path = require('path');
-    const http = require('http');
-    const https = require('https');
-    const crypto = require('crypto');
-    // if users using http://, notify them to use https://
-    
-  }
-} 
+const {
+  Git, GitBuilder, GitServer, GitStatus
+} = require('./lib/index.js');
+const {
+  GitError, GitErrorType, GitErrorCode
+} = require('./lib/errors.js');
+if (typeof module !== 'undefined' && module.exports){
+  module.exports = {
+    Git, GitBuilder, GitServer, GitStatus,
+    GitError, GitErrorType, GitErrorCode
+  };
+} else {
+  window.Git = {
+    Git, GitBuilder, GitServer, GitStatus,
+    GitError, GitErrorType, GitErrorCode
+  };
+}
+if (typeof window !== 'undefined'){
+  throw new Error('window is not defined');
+}
